17eb38aedac3fc5c16aec0dd215b2460a0248889
Diegololop <132150401+Diegololop@users.noreply.github.com>
2024-04-02 15:15:33 -0300

    jscontactOK

--- js/jsContact.js
+++ js/jsContact.js
@@ -1,14 +1,9 @@
 document.getElementById("contactForm").addEventListener("submit", function(event) {
     event.preventDefault(); // Prevenir el envío del formulario por defecto
     
-    // Muestra el mensaje de confirmación
-    document.getElementById("confirmation").style.display = "block";
-    
+    // Muestra un mensaje de alerta
+    alert("¡Gracias por tu mensaje! Te contactaremos pronto.");
+
     // Borra el contenido del formulario
     document.getElementById("contactForm").reset();
-
-    // Espera 3 segundos antes de ocultar el mensaje de confirmación
-    setTimeout(function() {
-        document.getElementById("confirmation").style.display = "none";
-    }, 3000);
 });
